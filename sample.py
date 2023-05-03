184a0c489fdff6a41e6ec00be68fe43a9e82ed92
polymorphisma <shrawansunar.6@gmail.com>
2023-05-03 08:30:00 +0545

    sample.py UPDATED

--- sample.py
+++ sample.py
@@ -1,9 +1,9 @@
 from nepse_scraper import Request_module
 
-# create object from Request_module class
-request_obj = Request_module()
+if __name__ == '__main__':
+    # create object from Request_module class
+    request_obj = Request_module()
 
-# getting today's price from nepse
-today_price = request_obj.get_today_price()
-print(today_price)
-
+    # getting today's price from nepse
+    today_price = request_obj.get_today_price()
+    print(today_price)
