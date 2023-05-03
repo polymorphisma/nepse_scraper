d66624f2a94a93b87f32bd52deeb03438ea1a6fa
polymorphisma <shrawansunar.6@gmail.com>
2023-05-03 13:26:26 +0545

    new version 0.1.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os import path
 
 current_dir = path.abspath(path.dirname(__file__))
 
-VERSION = '0.1.3'
+VERSION = '0.1.4'
 DESCRIPTION = 'Python Scraper for Nepse'
 
 with open(path.join(current_dir, 'README.md'), encoding='utf-8') as f:
@@ -15,6 +15,7 @@ setup(
     version=VERSION,
     author="Shrawan Sunar",
     author_email="shrawansunar.6@gmail.com",
+    package_data={'nepse_scraper': ['*.wasm']},
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
@@ -22,7 +23,7 @@ setup(
     url='https://github.com/polymorphisma/nepse_scraper/',  
     install_requires=['requests','urllib3','wasmtime', 'retrying'],
     keywords=['python', 'nepse data', 'nepse scraper'],
-    download_url="https://github.com/polymorphisma/nepse_scraper/archive/refs/tags/v_01.3.tar.gz",
+    download_url="https://github.com/polymorphisma/nepse_scraper/archive/refs/tags/v_01.4.tar.gz",
     classifiers=[
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
